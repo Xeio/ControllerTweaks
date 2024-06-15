06507b044487116fa5ca322d357f2127d912031c
Joshua Shaffer <xeio@live.com>
2024-06-15 14:16:40 -0400

    Fix item tooltip issue with Golden Road

--- Source/Tooltips.lua
+++ Source/Tooltips.lua
@@ -11,7 +11,36 @@ local showTTTCPriceOption = {
     default = true
 }
 
-Tooltips.options = { showTTTCPriceOption }
+local showExpandedTTCPriceInfo = {
+    settingKey = "ShowExpandedTTCPrice",
+    type = "checkbox",
+    name = "Show Expanded TTC Price Info",
+    tooltip = "Shows all TTC info like suggested %, min, max, and listings.",
+    width = "full",
+    default = false
+}
+
+Tooltips.options = { showTTTCPriceOption, showExpandedTTCPriceInfo }
+
+local function AddExpandedPriceInfo(tooltip, itemLink, name, priceInfo)
+    if not CT.Settings[showExpandedTTCPriceInfo.settingKey] then
+        return
+    end
+
+    local bottomStyle =
+    {
+        customSpacing = 10,
+        childSpacing = 10,
+        widthPercent = 100,
+        fontColorField = GENERAL_COLOR_OFF_WHITE,
+        fontFace = "$(GAMEPAD_LIGHT_FONT)",
+    }
+
+    local bottomSection = tooltip:AcquireSection(bottomStyle)
+    bottomSection:AddLine(string.format("Min:%s Avg:%s Max:%s", ZO_CurrencyControl_FormatCurrency(priceInfo.Min, true, nil), ZO_CurrencyControl_FormatCurrency(priceInfo.Avg, true, nil), ZO_CurrencyControl_FormatCurrency(priceInfo.Max, true, nil)))
+    bottomSection:AddLine(string.format("%s listings", priceInfo.EntryCount))
+    tooltip:AddSection(bottomSection)
+end
 
 local function AddPriceLine(tooltip, itemLink, name)
     if not CT.Settings[showTTTCPriceOption.settingKey] then
@@ -33,36 +62,10 @@ local function AddPriceLine(tooltip, itemLink, name)
 
         local section = tooltip:AcquireSection(style)
         local suggested = priceInfo.SuggestedPrice or priceInfo.Min
-        section:AddLine("TTC")
-        section:AddLine(ZO_CurrencyControl_FormatCurrency(suggested, true, nil))
+        section:AddLine("TTC " .. ZO_CurrencyControl_FormatCurrency(suggested, true, nil))
         tooltip:AddSection(section)
-    end
-end
 
-local function AddBottomTooltip(tooltip, itemLink, name)
-    if not CT.Settings[showTTTCPriceOption.settingKey] then
-        return
-    end
-    local priceInfo = TamrielTradeCentrePrice:GetPriceInfo(itemLink)
-    if priceInfo then
-        local bottomStyle =
-        {
-            customSpacing = 30,
-            childSpacing = 10,
-            widthPercent = 100,
-            fontColorField = GENERAL_COLOR_OFF_WHITE,
-            fontFace = "$(GAMEPAD_LIGHT_FONT)",
-        }
-
-        local bottomSection = tooltip:AcquireSection(bottomStyle)
-        if(priceInfo.SuggestedPrice) then
-            bottomSection:AddLine(string.format("TTC Suggested %s", ZO_CurrencyControl_FormatCurrency(priceInfo.SuggestedPrice, true, nil)))
-        else
-            bottomSection:AddLine("TTC")
-        end
-        bottomSection:AddLine(string.format("Min:%s Avg:%s Max:%s", ZO_CurrencyControl_FormatCurrency(priceInfo.Min, true, nil), ZO_CurrencyControl_FormatCurrency(priceInfo.Avg, true, nil), ZO_CurrencyControl_FormatCurrency(priceInfo.Max, true, nil)))
-        bottomSection:AddLine(string.format("%s listings", priceInfo.EntryCount))
-        tooltip:AddSection(bottomSection)
+        AddExpandedPriceInfo(tooltip, itemLink, name, priceInfo)
     end
 end
 
@@ -71,7 +74,6 @@ function Tooltips:Init()
         return
     end
     ZO_PostHook(GAMEPAD_TOOLTIPS:GetAndInitializeTooltipContainerTip(GAMEPAD_LEFT_TOOLTIP).tooltip, "AddTopSection", AddPriceLine)
-    ZO_PostHook(GAMEPAD_TOOLTIPS:GetAndInitializeTooltipContainerTip(GAMEPAD_LEFT_TOOLTIP).tooltip, "LayoutItem", AddBottomTooltip)
 end
 
 CT.Tooltips = Tooltips
